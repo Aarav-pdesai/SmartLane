908331f13b062fc3a6799d64cd8a7e7224523ae4
aarav desai <Aarav desai>
2024-06-29 23:52:35 +0530

    added table creation code

--- assets/database.py
+++ assets/database.py
@@ -1,3 +1,16 @@
 '''
+DATABASE AND TABLE CREATION CODE:
+
+CREATE DATABASE license_plates_db;
+USE license_plates_db;
+
+CREATE TABLE IF NOT EXISTS license_plates (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    frame_number INT,
+    car_id INT,
+    license_plate VARCHAR(20),
+    timestamp TEXT,
+    vehicle_type VARCHAR(50)
+);
 
 '''
